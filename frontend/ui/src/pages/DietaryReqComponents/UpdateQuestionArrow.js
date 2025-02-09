f4e92f028ad7abb0aa637cea41201626e58663d4
MiricleDev <rohan13k@outlook.com>
2025-02-09 02:25:50 +0000

    j

--- frontend/ui/src/pages/DietaryReqComponents/UpdateQuestionArrow.js
+++ frontend/ui/src/pages/DietaryReqComponents/UpdateQuestionArrow.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 import { IconContext } from 'react-icons/lib';
 
-const UpdateQuestionArrow = ({questionNumber, increment, decrement}) => {
+const UpdateQuestionArrow = ({questionNumber, increment, decrement }) => {
   
     return(
         <IconContext.Provider value={{size: '2rem'}}>
             <div className='container'>
                 { questionNumber > 1 && <button className='invisible-btn-left' onClick={decrement}><FaArrowCircleLeft /></button>}
-                { questionNumber < 3 && <button className='invisible-btn-right' onClick={increment}><FaArrowCircleRight /></button>}
+                { questionNumber < 4 && <button className='invisible-btn-right' onClick={increment}><FaArrowCircleRight /></button>}
             </div>
         </IconContext.Provider>
 
--- frontend/ui/src/pages/DietaryRequirements.js
+++ frontend/ui/src/pages/DietaryRequirements.js
@@ -4,11 +4,13 @@ import './index.css'
 import SelectableDiv from './DietaryReqComponents/SelectableDiv';
 import Slider from './DietaryReqComponents/Slider';
 import UpdateQuestionArrow from './DietaryReqComponents/UpdateQuestionArrow';
+import { CSSTransition } from "react-transition-group";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const DietaryRequirements = () => {
     const cuisines = ['South Asian', 'Italian', 'Mediterranean', 'Chinese', 'Middle-Eastern', 'Other']
 
-    const cuisineElements = cuisines.map(cuisine => (<SelectableDiv>{cuisine}</SelectableDiv>))
 
     const [question, setQuestion] = useState(1)
 
@@ -20,6 +22,45 @@ const DietaryRequirements = () => {
       setQuestion(prevQuestion => prevQuestion - 1)
     }
 
+    const [selectedCuisines, setSelectedCuisines] = useState([])
+
+    const navigate = useNavigate()
+
+    const dietaryRequirements = useRef()
+
+    // POST REQUEST------------------------------------------------------------------
+    useEffect(() => {
+        if(question >= 4){
+
+            axios.post('http://localhost:5000/api/recipes/filter', {
+                cuisines,
+                dietaryRequirements: dietaryRequirements.current.value
+            })
+
+            navigate('/mealplan/')
+        }
+    }, [question])
+    // --------------------------------------------------------------------------------
+
+    const updateSelectedCuisines = (name) => {
+        if(!selectedCuisines.includes(name)){
+            setSelectedCuisines(prevSelectedCuisines => [...prevSelectedCuisines, name])
+        } else{
+            setSelectedCuisines(prevSelectedCuisines => prevSelectedCuisines.filter(cuisine => cuisine !== name))
+        }
+    }
+
+    useEffect(() => {
+        console.log(selectedCuisines); // Logs the correct updated value
+      }, [selectedCuisines]);
+
+    const cuisineElements = cuisines.map((cuisine) => (<SelectableDiv 
+        updateSelectedCuisines={updateSelectedCuisines} 
+        name={cuisine}
+        >{cuisine}
+    </SelectableDiv>))
+
+
     return (
         <div className='bg center-container '>
             <div className='dtrequirements-form'>
@@ -29,7 +70,11 @@ const DietaryRequirements = () => {
                             <div className='floating-selectable-divs'>
                                 {cuisineElements} 
                             </div>
-                            <UpdateQuestionArrow questionNumber={question} increment={incrementPage} decrement={decrementPage} />
+                            <UpdateQuestionArrow 
+                                questionNumber={question} 
+                                increment={incrementPage} 
+                                decrement={decrementPage}
+                             />
                         </div>
                     )}
                     {question == 2 && (
@@ -42,7 +87,7 @@ const DietaryRequirements = () => {
                     {question == 3 && (
                         <div>
                             <h3>What are your dietary requirements?</h3>
-                            <textarea />
+                            <textarea ref={dietaryRequirements} />
                             <UpdateQuestionArrow questionNumber={question} increment={incrementPage} decrement={decrementPage} />
                         </div>
                     )}
